02835c9e57e6066777be50b9df563d3d53f0b593
Pasith Prayoonrat <pp1922@ic.ac.uk>
2022-10-14 10:03:37 +0100

    Update on README

--- README.md
+++ README.md
@@ -4,15 +4,9 @@
 
 *Created: Oct 2022*
 
-This repository contains all the work attempted by Pasith within the Computational Methods in Ecology and Evolution (CMEE) Course.
-
-Visitors who wish to attempt the coursework can refer to https://mhasoba.github.io/TheMulQuaBio/intro.html
-
-Each individual subdirectory will contain their own READMEs detailing the contents within the week. Also located within each subdirectories are another 3 subdirectories /Code, /Results and /Data. /Code contains the scripts, /Results contains the output from the scripts and /Data contains input files for the scripts. One point to highlight is that if input files are only manipulated (e.g. changing file type and/or replacing separators), the output remains in /Data. Outputs are only directed to /Results when the input files were processed (e.g. generating a graph from a dataset).
+THIS IS THE DESCRIPTION (add later)
 
 ## Table of Contents
 1. Week 1: UNIX and Linux, shell scripting, git and Latex
 2. Week 2: Biological Computing in Python
-3. Week 3: Biological Computing in R
-4. Week 7: Biological Computing in Python II
-5. MiniProject
+
--- Week2/code/cfexercises1.py
+++ Week2/code/cfexercises1.py
@@ -1,13 +1,20 @@
+__author__ = 'Pasith Prayoorat(pp1922@ic.ac.uk'
+__version__ = '0.0.1'
 
-def foo_1(x):
+import sys #import the system
+
+def foo_1(x=5):
+    """Calculate the square root of x"""
     return x ** 0.5
 
-def foo_2(x, y):
+def foo_2(x=5, y=2):
+    """If x is more than y, return y"""
     if x > y:
         return x
     return y
 
-def foo_3(x, y, z):
+def foo_3(x=4, y=5, z=6):
+    """ Put x, y, and z in an order from lowest to the highest."""
     if x > y:
         tmp = y
         y = x
@@ -18,18 +25,21 @@ def foo_3(x, y, z):
         y = tmp
     return [x, y, z]
 
-def foo_4(x):
+def foo_4(x=6):
+    """Factorial of x"""
     result = 1
     for i in range(1, x + 1):
         result = result * i
     return result
 
-def foo_5(x): # a recursive function that calculates the factorial of x
+def foo_5(x=10):
+    """ A recursive function that calculates the factorial of x """
     if x == 1:
         return 1
     return x * foo_5(x - 1)
      
-def foo_6(x): # Calculate the factorial of x in a different way; no if statement involved
+def foo_6(x=5):
+    """Calculate the factorial of x in a different way; no if statement involved """
     facto = 1
     while x >= 1:#
         facto = facto * x
